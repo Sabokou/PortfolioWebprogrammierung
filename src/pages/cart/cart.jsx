6c3598eae79d3589bf2893ea4d363fd2ccbfc2eb
Phillip Lange <phillip@lange-privat.net>
2021-02-04 14:14:02 +0100

    added default for seconday pages

---
+++ src/pages/cart/cart.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+//import Products from '../.././components/Products/Product'
+import './cart.css'
+
+const cart = () => {
+    return (
+        <div class = "flexContainer">
+           Warenkorbseite
+        </div>
+    )
+}
+
+export default cart
---
+++ src/pages/checkout/checkout.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+//import Products from '../.././components/Products/Product'
+import './checkout.css'
+
+const checkout = () => {
+    return (
+        <div class = "flexContainer">
+           Checkout 
+        </div>
+    )
+}
+
+export default checkout
