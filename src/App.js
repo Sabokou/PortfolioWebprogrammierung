d104e5e49417e12f45f54a49247a153c9b4ff908
Phillip Lange <phillip@lange-privat.net>
2021-02-04 14:12:50 +0100

    added cart functionality

--- src/App.js
+++ src/App.js
@@ -1,29 +1,72 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState } from 'react';
 import {BrowserRouter as Router, Route, NavLink} from "react-router-dom";
 
-import Products from'./components/Products/Products';
-
+//import Products from'./components/Products/Products';
+import productlist from './components/Products/productlist'
 
 import './App.css';
-import './pages/cart/cart';
-import './pages/checkout/checkout';
-import './pages/home/home';
+
+import Homepage from './pages/home/home';
+import checkout from './pages/checkout/checkout';
+import cart from './pages/cart/cart';
+
+
 
 const App = () => {
   
-  const [products, setProducts] = useState([]);
+  const {product} = productlist
+  const [cartItems, setCartItems] = useState([]);
+  
+  
+  const AddToCart = (product) => {
+    const exist = cartItems.find((x) => x.id === product.id);
+    let Menge = document.getElementById(product.name).value;
+    console.log(Menge);
+    if (exist){
+      setCartItems(
+        cartItems.map((x) =>
+        x.id === product.id ? { ...exist, qty: parseInt(exist.qty)+parseInt(Menge) } : x
+        )
+      );
+    } else {
+      setCartItems([...cartItems, {...product, qty: (Menge? Menge : 1)}])
+    }
+  };
+
+  const IncreaseItemCount = (product) => {
+    const exist = cartItems.find((x) => x.id === product.id);
+      setCartItems(
+        cartItems.map((x) =>
+        x.id === product.id ? { ...exist, qty: parseInt(exist.qty)+parseInt(1) } : x
+        )
+      );
+  };
+
+  const ReduceItemCount = (product) => {
+    const exist = cartItems.find((x) => x.id === product.id);
+    if (exist.qty <= 1){
+      setCartItems(cartItems.filter((x) => x.id !== product.id));
+    } else {
+      setCartItems(
+        cartItems.map((x) =>
+        x.id === product.id ? { ...exist, qty: parseInt(exist.qty)-parseInt(1) } : x
+        )
+      );
+    } 
+  };
+
+  const DeleteItem = (product) => {
+    const exist = cartItems.find((x) => x.id === product.id);
+    setCartItems(cartItems.filter((x) => x.id !== product.id));
+  }
   
   return (
-    <div class = "flexContainer">
-      <Products />
-    </div>
+    <Router>
+      <Route path='/' exact component = {(props) => <Homepage {...props} countCartItems={cartItems.length} AddToCart={AddToCart}/> }/>
+      <Route path='/cart' exact component = {(props) => <checkout {...props} Löschen={DeleteItem} Plus={IncreaseItemCount} Minus = {ReduceItemCount} cartItems={cartItems}/>}/>
+      <Route path='/checkout' exact component = {(props) => <cart {...props} cartItems={cartItems}/>}/>
+    </Router>
   );
 }
 
 export default App;
-/*
-<Router>
-      <Route path='/' exact component = {home}/>
-      <Route path='/cart' exact component = {cart}/>
-      <Route path='/checkout' exact component = {checkout}/>
-    </Router>*/
