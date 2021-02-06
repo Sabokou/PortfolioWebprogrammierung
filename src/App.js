8446dae1d85fb9fe199339dddd77913b899bfbe6
Phillip Lange <phillip@lange-privat.net>
2021-02-06 13:28:48 +0100

    fixed React components need to be capitalized

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ import productlist from './components/Products/productlist'
 import './App.css';
 
 import Homepage from './pages/home/home';
-import checkout from './pages/checkout/checkout';
-import cart from './pages/cart/cart';
+import Checkout from './pages/checkout/checkout';
+import Cart from './pages/cart/cart';
 
 
 
@@ -19,10 +19,14 @@ function App () {
   
   
   const AddToCart = (product) => {
-    console.log("tried adding item", product.id)
+    
+    console.log("Adding item:", product.id)
+    
     const exist = cartItems.find((x) => x.id === product.id);
     let amount = document.getElementById(product.name).value;
+    
     console.log(amount);
+    
     if (exist){
       setCartItems(
         cartItems.map((x) =>
@@ -36,15 +40,16 @@ function App () {
 
   const IncreaseItemCount = (product) => {
     const exist = cartItems.find((x) => x.id === product.id);
-      setCartItems(
-        cartItems.map((x) =>
-        x.id === product.id ? { ...exist, qty: parseInt(exist.qty)+parseInt(1) } : x
-        )
-      );
+    setCartItems(
+      cartItems.map((x) =>
+      x.id === product.id ? { ...exist, qty: parseInt(exist.qty)+parseInt(1) } : x
+      )
+    );
   };
 
   const ReduceItemCount = (product) => {
     const exist = cartItems.find((x) => x.id === product.id);
+    
     if (exist.qty <= 1){
       setCartItems(cartItems.filter((x) => x.id !== product.id));
     } else {
@@ -59,13 +64,17 @@ function App () {
   const DeleteItem = (product) => {
     const exist = cartItems.find((x) => x.id === product.id);
     setCartItems(cartItems.filter((x) => x.id !== product.id));
-  }
+  };
+
+  const DeleteAll = (product) => {
+    setCartItems(cartItems.filter((x) => x.id !== x.id))
+  };
   
   return (
     <Router>
       <Route path='/' exact component = {(props) => <Homepage {...props} countCartItems={cartItems.length} AddToCart={AddToCart}/> }/>
-      <Route path='/cart' exact component = {(props) => <checkout {...props} Löschen={DeleteItem} Plus={IncreaseItemCount} Minus = {ReduceItemCount} cartItems={cartItems}/>}/>
-      <Route path='/checkout' exact component = {(props) => <cart {...props} cartItems={cartItems}/>}/>
+      <Route path='/cart' exact component = {(props) => <Cart {...props} Delete={DeleteItem} AddItem={IncreaseItemCount} DecreaseItem = {ReduceItemCount} cartItems={cartItems}/>}/>
+      <Route path='/checkout' exact component = {(props) => <Checkout {...props} cartItems={cartItems}/>}/>
     </Router>
   );
 }
