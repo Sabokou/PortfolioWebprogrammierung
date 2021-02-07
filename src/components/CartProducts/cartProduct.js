ba807912c5b29064a478649e3bed370d2979c620
Phillip Lange <phillip@lange-privat.net>
2021-02-07 17:31:00 +0100

    adapted original product.js for the cart page

--- src/components/CartProducts/cartProduct.js
+++ src/components/CartProducts/cartProduct.js
@@ -4,20 +4,23 @@ import {BrowserRouter as Router, Route, NavLink} from "react-router-dom";
 
 
 function GenerateCart(key, cartItem, AddItem, DecreaseItem, DeleteItem) {
-    console.log(key, "generate_products called")
+
+    console.log(key, "Preis:", cartItem.price)
     return (
-        <div className="card">
-            <div className="cardContent">
-                <img className="cardImage" src = {cartItem.image} alt = 'Bild'/>
-                <div className = "cardProductName">{cartItem.name}</div>
-                <div className = "cardProductDescription">{cartItem.description}</div>
-            </div>
-            <div className="cardButtonRow">
-                <h5>Preis:{cartItem.price}</h5>
-                <div className="item"><input type="number" id={cartItem.name} min="1" max="100" /></div>
-                <button className="cardButtonShopping" onClick={() => DeleteItem(cartItem)}></button>
-            </div>
-        </div> 
+        <div className="productRow">
+            <button className = "button delete" onClick = {() => DeleteItem(cartItem)}>X</button>
+            <button className = "button add" onClick = {() => AddItem(cartItem)}>+</button>
+            <button className = "button subtract"onClick = {() => DecreaseItem(cartItem)}>-</button>
+            
+            <div className = "amount">{cartItem.qty}</div>
+            <div className = "productName">{cartItem.name}</div>
+
+            <div className = "productPrice">{cartItem.price}</div>
+            <div className = "productTotal">{cartItem.qty}</div>
+                
+        
+        </div>
+         
     )
 }
 
