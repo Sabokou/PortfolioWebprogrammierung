0470ce71c089b379dd92583ebbf39815f5f28e45
Phillip Lange <phillip@lange-privat.net>
2021-02-10 16:17:47 +0100

    designed Cart site

--- src/pages/cart/cart.js
+++ src/pages/cart/cart.js
@@ -1,18 +1,42 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
+
 
 import Navbar from '../.././components/Navbar/navbar'
 import GenerateCart from '../.././components/CartProducts/cartProduct'
+import PageTitle from '../.././components/PageTitle/pagetitle'
+import CheckSum from '../.././components/CheckSum/checksum'
 import './cart.css'
 
 class Cart extends React.Component {
-    render() {
+    render() 
+    {
         const {DeleteItem, AddItem , DecreaseItem, cartItems} = this.props;
+        console.log(cartItems);
         return (
-            <div className="flexContainer">
-                <Navbar amount = {cartItems.length} />              
-                <div className="flexContainer">
-                    {cartItems.map(cartItems =>  GenerateCart(cartItems.id, cartItems, AddItem, DecreaseItem, DeleteItem))}
-                </div>
+            <div className="gridContainer">
+                <Navbar amount = {cartItems.length} />
+                
+                {cartItems.length > 0 ?
+                    <>
+                        <div className="flexContainer">
+                            {cartItems.map(cartItems =>  GenerateCart(cartItems.name, cartItems, AddItem, DecreaseItem, DeleteItem))}
+                        </div>
+                        <div className = "purchaseButton">
+                            {<NavLink to = "/checkout">
+                                <button>Jetzt Kaufen &#10132;</button>
+                            </NavLink>}
+                        </div>
+                        <div className = "sumField">
+                            <CheckSum cartItems = {cartItems} />
+                        </div>
+                    </>
+                    :
+                    <>
+                    <div className = "noEntry"><h1>Noch keine Elemente im Warenkorb! Füg doch welche hinzu &#128512;</h1></div> 
+                    </>
+                }
+                
             </div> 
  
         );
