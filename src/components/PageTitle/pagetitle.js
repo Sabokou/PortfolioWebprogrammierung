9be9c91eb269608481a802edffa462eeec42e910
Phillip Lange <phillip@lange-privat.net>
2021-02-10 16:17:03 +0100

    WIP added pagetitle component analog to prototype

---
+++ src/components/PageTitle/pagetitle.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import './pagetitle.css'
+
+function PageTitle (title) {
+    return (<div className = "pagetitle">
+                {title}        
+            </div>)
+   
+};
+
+export default PageTitle;
