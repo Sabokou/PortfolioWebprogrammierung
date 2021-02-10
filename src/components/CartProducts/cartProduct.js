d85c977efa6de0a47b0672fd2c141c717ec0e541
Phillip Lange <phillip@lange-privat.net>
2021-02-10 16:16:23 +0100

    changed CSS

--- src/components/CartProducts/cartProduct.js
+++ src/components/CartProducts/cartProduct.js
@@ -8,15 +8,15 @@ function GenerateCart(key, cartItem, AddItem, DecreaseItem, DeleteItem) {
     console.log(key, "Preis:", cartItem.price)
     return (
         <div className="productRow">
-            <button className = "button delete" onClick = {() => DeleteItem(cartItem)}>X</button>
-            <button className = "button add" onClick = {() => AddItem(cartItem)}>+</button>
-            <button className = "button subtract"onClick = {() => DecreaseItem(cartItem)}>-</button>
+            <button className = "button margins border delete" onClick = {() => DeleteItem(cartItem)}>X</button>
+            <button className = "button margins border add" onClick = {() => AddItem(cartItem)}>+</button>
+            <button className = "button margins border subtract"onClick = {() => DecreaseItem(cartItem)}>-</button>
             
-            <div className = "amount">{cartItem.qty}</div>
-            <div className = "productName">{cartItem.name}</div>
-
-            <div className = "productPrice">{cartItem.price}</div>
-            <div className = "productTotal">{cartItem.qty}</div>
+            <div className = "amount border margins">{cartItem.qty}x</div>
+            <div className = "productPrice border margins">{cartItem.price}</div>
+            
+            <div className = "productName border margins">{cartItem.name}</div>
+            <div className = "productTotal border margins">&#931; {(cartItem.qty * parseFloat(cartItem.price.substring(0, cartItem.price.length - 1))).toFixed(2)}€</div>
                 
         
         </div>
