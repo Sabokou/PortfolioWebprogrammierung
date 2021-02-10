4b4e9b3f91d72705a5cd4d6a09ba7ea9ca6c3ee4
Phillip Lange <phillip@lange-privat.net>
2021-02-10 16:16:39 +0100

    added sum component

---
+++ src/components/CheckSum/checksum.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import './checksum.css'
+
+function CheckSum({cartItems}) {
+    //console.log(cartItems);
+    let sum = cartItems.reduce((total,item) => total + parseFloat(item.price.substring(0, item.price.length - 1)) * item.qty,0);
+    console.log(sum)
+    //cartItems.forEach(item => console.log(item.price));
+    return (
+        <div className = "sumField">
+           {sum}&euro; 
+        </div>
+    )
+}
+
+export default CheckSum;
