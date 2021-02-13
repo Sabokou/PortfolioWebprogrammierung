cba121a19d64a438af76e6b1ad5944a86a8532bd
Sabokou <52411026+Sabokou@users.noreply.github.com>
2021-02-13 15:39:08 +0100

    renamed file and moved product details

---
+++ src/components/CartRow/cartRow.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+import CartProducts from '../CartProducts/cartProducts'
+
+import './cartRow.css';
+
+
+
+function GenerateCart(key, cartItem, AddItem, DecreaseItem, DeleteItem) {
+    //console.log("Called Generate cart with ID:", key, "Price of Item:", cartItem.price)
+    console.log("GC-Item", cartItem)
+    console.log(cartItem.qty)
+    let productsDiv = CartProducts(cartItem.id, cartItem)
+    return (
+        <div className="productRow">
+            <button className = "button margins border delete" onClick = {() => DeleteItem(cartItem)}>X</button>
+            <button className = "button margins border add" onClick = {() => AddItem(cartItem)}>+</button>
+            <button className = "button margins border subtract" onClick = {() => DecreaseItem(cartItem)}>-</button>
+            <div className = "productDetails">
+                {productsDiv}
+            </div>                
+        
+        </div>
+         
+    )
+}
+
+export default GenerateCart;
