c4162323d5ceb082a18e28eda9e06b2d8cbce47c
Sabokou <52411026+Sabokou@users.noreply.github.com>
2021-02-14 14:34:46 +0100

    finished functional component of checkout page

--- src/pages/checkout/checkout.js
+++ src/pages/checkout/checkout.js
@@ -1,23 +1,58 @@
 import React from 'react';
-import {NavLink} from "react-router-dom";
+import { withRouter } from "react-router-dom";
 
 import CartProducts  from '../.././components/CartProducts/cartProducts'
 import Navbar from '../.././components/Navbar/navbar'
 
 import './checkout.css'
 
-function Clicked(DeleteAll) {
-    DeleteAll();
-    alert("Kauf erfolgreich.\nKehre zur Startseite zurück.")
-};
+function getAllIndexes(arr, val) {
+    var indexes = [], i;
+    for(i = 0; i < arr.length; i++)
+        if (arr[i] === val)
+            indexes.push(i+1);
+    return indexes;
+}
 
-function CheckEntries(DeleteAll){
-    console.log();
-};
+
 
 class Checkout extends React.Component {
     
-      
+    CheckEntries(DeleteAll){
+        let correct = [];
+    
+        for (let index = 1; index < 8; index++){
+            
+            //fetch value of html input field
+            let str = "input".concat(index.toString());
+            let value = document.getElementById(str).value;
+            
+            if ( value === "" ) {
+                correct.push("false")
+            } else {
+                correct.push("true")
+            }
+            
+        }
+        if ( correct.filter(x => x === "false").length === 0 ) {
+            DeleteAll();
+            alert("Kauf erfolgreich.\nKehre zur Startseite zurück.");
+            return true;
+        } else {
+            console.log(correct);
+            const str = "Fehlende Eingabe! Bitte Felder ".concat(getAllIndexes(correct, "false").join(), " überprüfen") 
+            alert(str);
+            return false;
+        }
+    };
+
+    registerSubmit(DeleteAll) {
+           
+        if (this.CheckEntries(DeleteAll)) {
+            this.props.history.push('/');
+        }
+    };
+
     render() {
         const {cartItems, DeleteAll} = this.props;
         console.log("checkout opened")
@@ -31,7 +66,7 @@ class Checkout extends React.Component {
                     <input className = "row1 column1" id = "input2" placeholder = "Vorname" />
                     <input className = "row2 column1" id = "input3" placeholder = "Straße" />
                     <input className = "row2 column2" id = "input4" type = "number" min = "1" placeholder = "Hausnummer" />
-                    <input className = "row3 column1" id = "input5" placeholder = "PLZ"/>
+                    <input className = "row3 column1" id = "input5" type = "number" placeholder = "PLZ"/>
                     <input className = "row3 column2" id = "input6" placeholder = "Ort"/>
                     <input className = "row4 columnspan" id = "input7" type = "email" />
 
@@ -39,13 +74,13 @@ class Checkout extends React.Component {
                     <div className = "products">
                         {cartItems.map(cartItems =>  CartProducts(cartItems.id, cartItems))}
                     </div>
-                    <NavLink to = "/"><button onClick = {() => Clicked(DeleteAll)}>Jetzt Kaufen!</button></NavLink>
+                    <button onClick = {() => this.registerSubmit(DeleteAll)}>Jetzt Kaufen!</button>
                 </div>
                     
             </div> 
- 
+
         );
     }
-  }
+};
 
-  export default Checkout;
+export default withRouter(Checkout);
